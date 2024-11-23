c7b84980cb832d4bb8a64827f6e8fa452473582f
Wenda Berutu <wendaberutu22@example.com>
2024-11-23 22:26:38 +0700

    membuat Broadcast

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
 class UserSeeder extends Seeder
@@ -14,15 +14,52 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'nama' =>'WendaAdmin',
-            'no_whatshap' =>'082273500223',
-            'email' =>'wendaadmin@example.com',
-            'password' => bcrypt('wenda123'),
-            'umur' => 30, // Ensure umur is set
-            'tanggal_lahir' => Carbon::createFromFormat('Y-m-d', '1994-05-20'),
-            'created_at' => now(),
-            'updated_at' => now(),
+        // Insert multiple users at once
+        User::insert([
+            [
+                'nama' => 'WendaAdmin',
+                'no_whatshap' => '082273500223',
+                'email' => 'wendaadmin@example.com',
+                'password' => bcrypt('wenda123'),
+                'alamat' => 'paingan',
+                'umur' => 30,
+                'tanggal_lahir' => Carbon::createFromFormat('Y-m-d', '1994-05-20'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Damar',
+                'no_whatshap' => '089623573070',
+                'email' => 'damar@example.com',
+                'password' => Hash::make('password123'),
+                'alamat' => 'Some Address',
+                'umur' => 17,
+                'tanggal_lahir' => Carbon::createFromFormat('Y-m-d', '2007-03-15'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Cinta',
+                'no_whatshap' => '083831739340',
+                'email' => 'cinta@example.com',
+                'password' => Hash::make('password123'),
+                'alamat' => 'Another Address',
+                'umur' => 25,
+                'tanggal_lahir' => Carbon::createFromFormat('Y-m-d', '1999-02-10'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Wendy',
+                'no_whatshap' => '082155495919',
+                'email' => 'wendy@example.com',
+                'password' => Hash::make('password123'),
+                'alamat' => 'Wendy\'s Address',
+                'umur' => 40,
+                'tanggal_lahir' => Carbon::createFromFormat('Y-m-d', '1984-07-01'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
